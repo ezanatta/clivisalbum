5610ecaa4533b056a449b9feb538669fc95172bc
ezanatta <emiliojbzanatta@gmail.com>
2020-04-04 15:24:56 -0300

    added more colors to vis

--- albumart.sh
+++ albumart.sh
@@ -12,7 +12,8 @@ do
     glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
 
     chafa --color-space=din99d current_art.png -c 240 --symbols=block+dot+stipple+hhalf+vhalf+ascii --fill none --work=9
-    convert current_art.png +dither -colors 5 -unique-colors txt: > current_colors
+    # visualizer colors
+    convert current_art.png +dither -colors 10 -unique-colors txt: > current_colors
     awk 'NR>=2 {print $3}' current_colors > awk_current_colors
     echo 'gradient=false' >> awk_current_colors
     cp awk_current_colors ~/.config/vis/colors/dark
