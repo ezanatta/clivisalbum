ad950f239a7a0bc6dfc2796efdcfd374baaa7197
ezanatta <emiliojbzanatta@gmail.com>
2020-04-04 18:03:20 -0300

    Now the cover only changes if the album changes, no cpu hog anymore, yey!

--- albumart.sh
+++ albumart.sh
@@ -4,10 +4,29 @@
 # By Emilio Zanatta
 #
 
+ALBUM="$(ncmpcpp --current-song %b -q)"
+ARTIST="$(ncmpcpp --current-song %a -q)"
+
+glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
+
+chafa --color-space=din99d current_art.png -c 240 --symbols=block+dot+stipple+hhalf+vhalf+ascii --fill none --work=9
+# visualizer colors
+convert current_art.png +dither -colors 10 -unique-colors txt: > current_colors
+awk 'NR>=2 {print $3}' current_colors > awk_current_colors
+echo 'gradient=false' >> awk_current_colors
+cp awk_current_colors ~/.config/vis/colors/dark
+
 for (( ; ;  ))
 do
-    ALBUM="$(ncmpcpp --current-song %b -q)"
+    ALBUM2="$(ncmpcpp --current-song %b -q)"
     ARTIST="$(ncmpcpp --current-song %a -q)"
+
+    if [ "$ALBUM2" = "$ALBUM" ]; then
+        ALBUM="$ALBUM2"
+        continue
+    fi
+
+    ALBUM="$ALBUM2"
 
     glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
 
@@ -18,5 +37,4 @@ do
     echo 'gradient=false' >> awk_current_colors
     cp awk_current_colors ~/.config/vis/colors/dark
 
-    sleep 30
 done
