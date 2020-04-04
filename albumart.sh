140ddc45c9d86cd08b1fbdb8ecc9eb206b48c753
ezanatta <emiliojbzanatta@gmail.com>
2020-04-04 18:51:24 -0300

    rewritten code to look better

--- albumart.sh
+++ albumart.sh
@@ -4,17 +4,26 @@
 # By Emilio Zanatta
 #
 
+get_cover() {
+
+    glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
+
+    chafa --color-space=din99d current_art.png -c 240 --symbols=block+dot+stipple+hhalf+vhalf+ascii --fill none --work=9
+
+}
+
+vis_colors() {
+    convert current_art.png +dither -colors 10 -unique-colors txt: > current_colors
+    awk 'NR>=2 {print $3}' current_colors > awk_current_colors
+    echo 'gradient=false' >> awk_current_colors
+    cp awk_current_colors ~/.config/vis/colors/dark
+}
+
 ALBUM="$(ncmpcpp --current-song %b -q)"
 ARTIST="$(ncmpcpp --current-song %a -q)"
 
-glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
-
-chafa --color-space=din99d current_art.png -c 240 --symbols=block+dot+stipple+hhalf+vhalf+ascii --fill none --work=9
-# visualizer colors
-convert current_art.png +dither -colors 10 -unique-colors txt: > current_colors
-awk 'NR>=2 {print $3}' current_colors > awk_current_colors
-echo 'gradient=false' >> awk_current_colors
-cp awk_current_colors ~/.config/vis/colors/dark
+get_cover
+vis_colors
 
 for (( ; ;  ))
 do
@@ -28,13 +37,6 @@ do
 
     ALBUM="$ALBUM2"
 
-    glyrc cover --artist "$ARTIST" --album "$ALBUM" -v=0 --write current_art.png
-
-    chafa --color-space=din99d current_art.png -c 240 --symbols=block+dot+stipple+hhalf+vhalf+ascii --fill none --work=9
-    # visualizer colors
-    convert current_art.png +dither -colors 10 -unique-colors txt: > current_colors
-    awk 'NR>=2 {print $3}' current_colors > awk_current_colors
-    echo 'gradient=false' >> awk_current_colors
-    cp awk_current_colors ~/.config/vis/colors/dark
-
+    get_cover
+    vis_colors
 done
